fabb1f4627f61a3aebe22411dca01c1d21bffd14
Alan <cdl.prodrifter.dk@hotmail.com>
2024-07-18 03:40:56 -0400

    Finished mobile version

--- scripts.js
+++ scripts.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // Add any JavaScript needed for interactions
+$(document).ready(function () {
+    $('#logoCarousel').carousel({
+        interval: false
+    });
+
+    $('a[href="#contact"]').on('click', function (event) {
+        event.preventDefault();
+        $('html, body').animate({
+            scrollTop: $('#contact').offset().top
+        }, 1000);
+    });
+
+    $('#testimonialCarousel').carousel({
+        interval: 5000
+    });
+
+    $('#featureCarousel').carousel({
+        interval: 5000
+    });
 });
